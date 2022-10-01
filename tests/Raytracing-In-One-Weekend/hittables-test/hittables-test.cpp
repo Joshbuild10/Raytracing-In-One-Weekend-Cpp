2a62bd56f55cb274c6bf467b62912b4f7b9fa38d
Joshbuild10 <88260146+Joshbuild10@users.noreply.github.com>
2022-10-01 22:04:09 +0100

    Add hittable object class and functionality, Add utility header.

---
+++ tests/Raytracing-In-One-Weekend/hittables-test/hittables-test.cpp
@@ -0,0 +1,61 @@
+#include "../../../source/Raytracing-In-One-Weekend/utility.h"
+#include "../../../source/Raytracing-In-One-Weekend/colour.h"
+#include "../../../source/Raytracing-In-One-Weekend/sphere.h"
+#include "../../../source/Raytracing-In-One-Weekend/hittable_list.h"
+
+#include <iostream>
+
+colour ray_colour(const ray& r, const hittable& world)
+{
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) // Returns the scaled normal of the nearest object (if any) hit
+    {
+	    return 0.5 * (rec.normal + colour(1, 1, 1));
+    }
+    vec3 unit_direction = unit_vector(r.direction()); // Normalises the vector
+    auto t = 0.5 * (unit_direction.y + 1.0); // Scales to range 0 <= t <= 1
+    return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0); // Linear interpolation algorithm
+}
+
+int main()
+{
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; // Progress indicator
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            colour pixel_colour = ray_colour(r, world);
+            write_colour(std::cout, pixel_colour); // Writes colour to output buffer
+        }
+    }
+    std::cerr << "\nDone.\n";
+}
