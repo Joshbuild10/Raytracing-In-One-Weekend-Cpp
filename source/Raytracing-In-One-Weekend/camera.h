79771bd015a1f3b10b9e13bb7c77cdc90bdab3c6
Joshbuild10 <88260146+Joshbuild10@users.noreply.github.com>
2022-10-10 11:29:24 +0100

    Add Positionable Camera Functionality

--- source/Raytracing-In-One-Weekend/camera.h
+++ source/Raytracing-In-One-Weekend/camera.h
@@ -12,8 +12,30 @@ private:
     vec3 vertical;
 
 public:
-    camera()
+    camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio) // vfov is vertical field of view (in degrees)
 	{
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2.0 * h;
+        auto viewport_width = aspect_ratio * viewport_height;
+
+        auto w = unit_vector(lookfrom - lookat);
+        auto u = unit_vector(cross(vup, w));
+        auto v = cross(w, u);
+
+        origin = lookfrom;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
+    }
+
+    ray get_ray(double s, double t) const
+	{
+        return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
+    }
+
+    camera() // Old constructor for test compatibility
+    {
         auto aspect_ratio = 16.0 / 9.0;
         auto viewport_height = 2.0;
         auto viewport_width = aspect_ratio * viewport_height;
@@ -23,11 +45,6 @@ public:
         horizontal = vec3(viewport_width, 0.0, 0.0);
         vertical = vec3(0.0, viewport_height, 0.0);
         lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
-    }
-
-    ray get_ray(double u, double v) const
-	{
-        return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
     }
 };
 #endif
---
+++ tests/Raytracing-In-One-Weekend/camera-test/camera-test.cpp
@@ -0,0 +1,82 @@
+#include "../../../source/Raytracing-In-One-Weekend/utility.h"
+#include "../../../source/Raytracing-In-One-Weekend/colour.h"
+#include "../../../source/Raytracing-In-One-Weekend/sphere.h"
+#include "../../../source/Raytracing-In-One-Weekend/hittable_list.h"
+#include "../../../source/Raytracing-In-One-Weekend/camera.h"
+#include "../../../source/Raytracing-In-One-Weekend/material.h"
+
+#include <iostream>
+
+colour ray_colour(const ray& r, const hittable& world, int depth)
+{
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return colour(0, 0, 0);
+
+    hit_record rec;
+    if (world.hit(r, 0.001, infinity, rec)) // Returns the scaled normal of the nearest object (if any) hit
+    {
+        ray scattered;
+        colour attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_colour(scattered, world, depth - 1);
+        return colour(0, 0, 0);
+    }
+    vec3 unit_direction = unit_vector(r.direction()); // Normalises the vector
+    auto t = 0.5 * (unit_direction.y + 1.0); // Scales to range 0 <= t <= 1
+    return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0); // Linear interpolation algorithm
+}
+
+int main()
+{
+
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
+
+    // World
+    auto R = cos(pi / 4);
+    hittable_list world;
+
+    auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(colour(0.8, 0.6, 0.2), 0.0);
+
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+
+    // Camera
+
+    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90, aspect_ratio);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush; // Progress indicator
+        for (int i = 0; i < image_width; ++i)
+        {
+            colour pixel_colour(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) // Samples each pixel multiple times and sums the values
+            {
+                // Generates a random x and y offset
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world, max_depth);
+            }
+            write_colour(std::cout, pixel_colour, samples_per_pixel); // Writes colour to output buffer
+        }
+    }
+    std::cerr << "\nDone.\n";
+}
